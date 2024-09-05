8e0f0ea994617284fa8dc508dc4ca3552e60152c
Lino Munoz <lino.munoz@intempo.co>
2024-09-05 17:05:45 -0500

    Se agregarn formularios al registro de vehiculos

--- src/app/components/registrar-datos-propietario/registrar-datos-propietario.component.ts
+++ src/app/components/registrar-datos-propietario/registrar-datos-propietario.component.ts
@@ -2,8 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-registrar-datos-propietario',
-  standalone: true,
-  imports: [],
   templateUrl: './registrar-datos-propietario.component.html',
   styleUrl: './registrar-datos-propietario.component.scss'
 })
--- src/app/components/registrar-datos-propietario/registrar-datos-propietario.module.ts
+++ src/app/components/registrar-datos-propietario/registrar-datos-propietario.module.ts
@@ -2,12 +2,41 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { RegistrarDatosPropietarioRoutingModule } from './registrar-datos-propietario-routing.module';
+import { FormsModule } from '@angular/forms';
+import { FloatlabelDemoRoutingModule } from 'src/app/demo/components/uikit/floatlabel/floatlabeldemo-routing.module';
+import { AutoCompleteModule } from 'primeng/autocomplete';
+import { CalendarModule } from 'primeng/calendar';
+import { ChipsModule } from 'primeng/chips';
+import { DropdownModule } from 'primeng/dropdown';
+import { InputMaskModule } from 'primeng/inputmask';
+import { InputNumberModule } from 'primeng/inputnumber';
+import { CascadeSelectModule } from 'primeng/cascadeselect';
+import { MultiSelectModule } from 'primeng/multiselect';
+import { InputGroupModule } from 'primeng/inputgroup';
+import { InputGroupAddonModule } from 'primeng/inputgroupaddon';
+import { InputTextModule } from 'primeng/inputtext';
+import { InputTextareaModule } from 'primeng/inputtextarea';
+import { RegistrarDatosPropietarioComponent } from './registrar-datos-propietario.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [RegistrarDatosPropietarioComponent],
   imports: [
     CommonModule,
+    FormsModule,
+		FloatlabelDemoRoutingModule,
+		AutoCompleteModule,
+		CalendarModule,
+		ChipsModule,
+		DropdownModule,
+		InputMaskModule,
+		InputNumberModule,
+		CascadeSelectModule,
+		MultiSelectModule,
+		InputTextareaModule,
+		InputTextModule,
+		InputGroupModule,
+		InputGroupAddonModule,
     RegistrarDatosPropietarioRoutingModule
   ]
 })

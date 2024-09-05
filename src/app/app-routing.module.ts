814f98f57bfec0a74390b7a9d4028e4e35dc2bb1
Diego <diego.calvera@intempo.co>
2024-09-05 15:54:30 -0500

    commit inicial del proyecto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,29 +1,138 @@
 import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { NotfoundComponent } from './demo/components/notfound/notfound.component';
-import { AppLayoutComponent } from "./layout/app.layout.component";
+import { AppLayoutComponent } from './components/layout/app.layout.component';
 
 @NgModule({
     imports: [
-        RouterModule.forRoot([
+        RouterModule.forRoot(
+            [
+                {
+                    path: '',
+                    component: AppLayoutComponent,
+                    children: [
+                        {
+                            path: '',
+                            loadChildren: () =>
+                                import(
+                                    './components/inicio/inicio.module'
+                                ).then((m) => m.InicioModule),
+                        },
+                        {
+                            path: 'registrar-datos-vehiculo',
+                            loadChildren: () =>
+                                import(
+                                    './components/registrar-datos-vehiculo/registrar-datos-vehiculo.module'
+                                ).then((m) => m.RegistrarDatosVehiculoModule),
+                        },
+                        {
+                            path: 'registrar-datos-propietario',
+                            loadChildren: () =>
+                                import(
+                                    './components/registrar-datos-propietario/registrar-datos-propietario.module'
+                                ).then(
+                                    (m) => m.RegistrarDatosPropietarioModule
+                                ),
+                        },
+                        {
+                            path: 'consultar-registro',
+                            loadChildren: () =>
+                                import(
+                                    './components/consultar-registro/consultar-registro.module'
+                                ).then((m) => m.ConsultarRegistroModule),
+                        },
+                        {
+                            path: 'comprar-soat',
+                            loadChildren: () =>
+                                import(
+                                    './components/comprar-soat/comprar-soat.module'
+                                ).then((m) => m.ComprarSoatModule),
+                        },
+                        {
+                            path: 'consultar-soat',
+                            loadChildren: () =>
+                                import(
+                                    './components/consultar-soat/consultar-soat.module'
+                                ).then((m) => m.ConsultarSoatModule),
+                        },
+                        {
+                            path: 'registrar-datos-vehiculo/:id',
+                            loadChildren: () =>
+                                import(
+                                    './components/registrar-datos-vehiculo/registrar-datos-vehiculo.module'
+                                ).then((m) => m.RegistrarDatosVehiculoModule),
+                        },
+                        {
+                            path: 'registrar-datos-propietario/:id',
+                            loadChildren: () =>
+                                import(
+                                    './components/registrar-datos-propietario/registrar-datos-propietario.module'
+                                ).then(
+                                    (m) => m.RegistrarDatosPropietarioModule
+                                ),
+                        },
+
+                        {
+                            path: 'uikit',
+                            loadChildren: () =>
+                                import(
+                                    './demo/components/uikit/uikit.module'
+                                ).then((m) => m.UIkitModule),
+                        },
+                        {
+                            path: 'utilities',
+                            loadChildren: () =>
+                                import(
+                                    './demo/components/utilities/utilities.module'
+                                ).then((m) => m.UtilitiesModule),
+                        },
+                        {
+                            path: 'documentation',
+                            loadChildren: () =>
+                                import(
+                                    './demo/components/documentation/documentation.module'
+                                ).then((m) => m.DocumentationModule),
+                        },
+                        {
+                            path: 'blocks',
+                            loadChildren: () =>
+                                import(
+                                    './demo/components/primeblocks/primeblocks.module'
+                                ).then((m) => m.PrimeBlocksModule),
+                        },
+                        {
+                            path: 'pages',
+                            loadChildren: () =>
+                                import(
+                                    './demo/components/pages/pages.module'
+                                ).then((m) => m.PagesModule),
+                        },
+                    ],
+                },
+                {
+                    path: 'auth',
+                    loadChildren: () =>
+                        import('./demo/components/auth/auth.module').then(
+                            (m) => m.AuthModule
+                        ),
+                },
+                {
+                    path: 'landing',
+                    loadChildren: () =>
+                        import('./demo/components/landing/landing.module').then(
+                            (m) => m.LandingModule
+                        ),
+                },
+                { path: 'notfound', component: NotfoundComponent },
+                { path: '**', redirectTo: '/notfound' },
+            ],
             {
-                path: '', component: AppLayoutComponent,
-                children: [
-                    { path: '', loadChildren: () => import('./demo/components/dashboard/dashboard.module').then(m => m.DashboardModule) },
-                    { path: 'uikit', loadChildren: () => import('./demo/components/uikit/uikit.module').then(m => m.UIkitModule) },
-                    { path: 'utilities', loadChildren: () => import('./demo/components/utilities/utilities.module').then(m => m.UtilitiesModule) },
-                    { path: 'documentation', loadChildren: () => import('./demo/components/documentation/documentation.module').then(m => m.DocumentationModule) },
-                    { path: 'blocks', loadChildren: () => import('./demo/components/primeblocks/primeblocks.module').then(m => m.PrimeBlocksModule) },
-                    { path: 'pages', loadChildren: () => import('./demo/components/pages/pages.module').then(m => m.PagesModule) }
-                ]
-            },
-            { path: 'auth', loadChildren: () => import('./demo/components/auth/auth.module').then(m => m.AuthModule) },
-            { path: 'landing', loadChildren: () => import('./demo/components/landing/landing.module').then(m => m.LandingModule) },
-            { path: 'notfound', component: NotfoundComponent },
-            { path: '**', redirectTo: '/notfound' },
-        ], { scrollPositionRestoration: 'enabled', anchorScrolling: 'enabled', onSameUrlNavigation: 'reload' })
+                scrollPositionRestoration: 'enabled',
+                anchorScrolling: 'enabled',
+                onSameUrlNavigation: 'reload',
+            }
+        ),
     ],
-    exports: [RouterModule]
+    exports: [RouterModule],
 })
-export class AppRoutingModule {
-}
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
-import { HashLocationStrategy, LocationStrategy, PathLocationStrategy } from '@angular/common';
+import {
+    HashLocationStrategy,
+    LocationStrategy,
+    PathLocationStrategy,
+} from '@angular/common';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { AppLayoutModule } from './layout/app.layout.module';
 import { NotfoundComponent } from './demo/components/notfound/notfound.component';
 import { ProductService } from './demo/service/product.service';
 import { CountryService } from './demo/service/country.service';
@@ -11,15 +14,21 @@ import { EventService } from './demo/service/event.service';
 import { IconService } from './demo/service/icon.service';
 import { NodeService } from './demo/service/node.service';
 import { PhotoService } from './demo/service/photo.service';
+import { AppLayoutModule } from './components/layout/app.layout.module';
 
 @NgModule({
     declarations: [AppComponent, NotfoundComponent],
     imports: [AppRoutingModule, AppLayoutModule],
     providers: [
         { provide: LocationStrategy, useClass: PathLocationStrategy },
-        CountryService, CustomerService, EventService, IconService, NodeService,
-        PhotoService, ProductService
+        CountryService,
+        CustomerService,
+        EventService,
+        IconService,
+        NodeService,
+        PhotoService,
+        ProductService,
     ],
     bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}

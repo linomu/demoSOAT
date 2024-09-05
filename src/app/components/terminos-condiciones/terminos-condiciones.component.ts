dd8e572898546b3af294bcf18ccda70fc6845816
Diego <diego.calvera@intempo.co>
2024-09-05 17:17:08 -0500

    boton de redireccion a inicio

--- src/app/components/terminos-condiciones/terminos-condiciones.component.ts
+++ src/app/components/terminos-condiciones/terminos-condiciones.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-terminos-condiciones',
-  standalone: true,
-  imports: [],
-  templateUrl: './terminos-condiciones.component.html',
-  styleUrl: './terminos-condiciones.component.scss'
+    selector: 'app-terminos-condiciones',
+    templateUrl: './terminos-condiciones.component.html',
+    styleUrl: './terminos-condiciones.component.scss',
 })
 export class TerminosCondicionesComponent {
-
+    constructor(private router: Router) {}
+    navegarAInicio() {
+        this.router.navigate(['/soat/']);
+    }
 }
--- src/app/components/terminos-condiciones/terminos-condiciones.module.ts
+++ src/app/components/terminos-condiciones/terminos-condiciones.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { TerminosCondicionesRoutingModule } from './terminos-condiciones-routing.module';
+import { TerminosCondicionesComponent } from './terminos-condiciones.component';
 
 @NgModule({
-    declarations: [],
+    declarations: [TerminosCondicionesComponent],
     imports: [CommonModule, TerminosCondicionesRoutingModule],
 })
 export class TerminosCondicionesModule {}

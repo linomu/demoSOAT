0ba23d4805cefb88dbdb590c371134ec7db65396
dario.riano <dario.riano@intempo.co>
2024-09-06 14:23:15 -0500

    Footer e imágenes iniciales

--- src/app/components/terminos-condiciones/terminos-condiciones.module.ts
+++ src/app/components/terminos-condiciones/terminos-condiciones.module.ts
@@ -1,8 +1,10 @@
+
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { TerminosCondicionesRoutingModule } from './terminos-condiciones-routing.module';
 import { TerminosCondicionesComponent } from './terminos-condiciones.component';
+
 
 @NgModule({
     declarations: [TerminosCondicionesComponent],
